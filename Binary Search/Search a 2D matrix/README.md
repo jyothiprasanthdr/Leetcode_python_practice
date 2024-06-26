b8aeb69a34c8fec8fb016beb1b281520684cc6e0
Jyothi Prasanth <jyothiprasanthdr@gmail.com>
2024-06-25 23:28:48 -0700

    Jun 25 practice

---
+++ Binary Search/Search a 2D matrix/README.md
@@ -0,0 +1,11 @@
+![alt text](<Screenshot 2024-06-25 at 11.28.15 PM.png>)
+
+Notes:
+TC: O(log m + log n)
+SC: O(1)
+
+
+1. Array approach will lead us to O(n^2)
+2. Running Binary Search on matrix using top and bot to find the row, thus finding the row will have (log m) time complexity
+3. Once the row is found, we run again the Binary Search on the selected row in step 2 and check if the value presents in the matrix. this will lead to (log n) time complexity.
+4. Overall TC would be (log m + log n)
---
+++ Binary Search/Search a 2D matrix/search_2d_mat.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        top, bot = 0, rows - 1
+
+        while top <= bot:
+            row = (bot + top) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+
+        if not top <= bot:
+            return False
+
+        row = (top + bot) // 2
+        l = 0
+        r = cols - 1
+        while l <= r:
+            mid = (r + l) // 2
+            if matrix[row][mid] < target:
+                l = mid + 1
+            elif matrix[row][mid] > target:
+                r = mid - 1
+            else:
+                return True
+        return False
