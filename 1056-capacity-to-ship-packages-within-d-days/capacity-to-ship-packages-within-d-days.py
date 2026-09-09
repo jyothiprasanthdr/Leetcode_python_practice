e34a0560f2959cd7572b8b42c1ab7d9758fd9264
Jyothi Prasanth <jyothiprasanthdr@gmail.com>
2026-09-09 11:07:33 -0700

    Time: 229 ms (32.50%) | Memory: 23.8 MB (67.05%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,34 +1,35 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        l, r =max(weights), sum(weights)
 
-        res=r
+        def canShip(capacity):
+
+            days_needed=1
+            current_weight=0    
+
+            for w in weights:
+
+                if current_weight+w>capacity:
+                    days_needed+=1
+                    current_weight=0
+
+                current_weight+=w
+            return days_needed<=days
+
+
+        l=max(weights)
+        r= sum(weights)
+
+        ans = 0
+
+
 
         while l<=r:
+                m = (l+r)//2
 
-            m = l + (r-l)//2
+                if canShip(m):
+                    ans=m
+                    r=m-1
+                else:
+                    l=m+1
 
-            if self.feasible(weights, days, m):
-                res=m
-                r=m-1
-            else:
-                l=m+1
-        return res
-    
-    def feasible(self, weights, d, cap):
-
-        days_needed=1
-        current_weight=0
-        for w in weights:
-
-            if current_weight + w >cap:
-                days_needed+=1
-                current_weight=w
-            else:
-                current_weight+=w
-        
-            if days_needed>d:
-                return False
-        return True
-
-        
+        return ans
